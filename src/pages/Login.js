0c03bebe6a277b2c7edc87b49f22a2ea7f67bdc0
DanielFariias <danielfariias15@gmail.com>
2021-11-14 17:00:42 -0300

    Created Login Page and stylization

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import './Login.css';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+// class Login extends React.Component {
+//   render() {
+//     return <div>Login</div>;
+//   }
+// }
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="container">
+        <h1>Trybe</h1>
+
+        <form onSubmit={ handleSubmit }>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            data-testid="password-input"
+            minLength="6"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </main>
+  );
+};
 
 export default Login;
